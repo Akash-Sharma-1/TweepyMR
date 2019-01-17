191728a85dc45e4fdc6247564320ad330e10c7fe
Vidit Jain <vidit1000@gmail.com>
2019-01-17 12:10:41 +0530

    Delete upload.py

--- upload.py
+++
@@ -1,8 +0,0 @@
-from firebase import firebase
-import json
-firebase=firebase.FirebaseApplication('https://tweepy-7845f.firebaseio.com/',None)
-path="narendramodi.json"
-file=open(path,'r')
-file=file.read()
-j=json.loads(file)
-result=firebase.put('/','Tweets',j)

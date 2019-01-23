d504f1c0dda0770f1950f02330f1d514065b67b1
Vidit Jain <vidit1000@gmail.com>
2019-01-23 14:02:49 +0530

    updated search.py

--- twitter_scraping-master/search.py
+++ twitter_scraping-master/search.py
@@ -1,9 +1,15 @@
 import json
+import time
 from firebase import firebase
 query=input()
 query=query.lower()
-firebase=firebase.FirebaseApplication('https://tweepy-7845f.firebaseio.com/',None)
-result=firebase.get('/Tweets',None)
+#firebase=firebase.FirebaseApplication('https://tweepy-7845f.firebaseio.com/',None)
+#result=firebase.get('/Tweets',None)
+start=time.time()
+path='narendramodi_short.json'
+result=open(path,'r')
+result=result.read()
+result=json.loads(result)
 ids=[]
 for i in range(len(result)):
 	tweet=result[i]
@@ -15,4 +21,6 @@ if(len(ids)==0):
 	print("No results found")
 else:
 	print(ids)
+print(len(ids))
+print(time.time()-start)
 
